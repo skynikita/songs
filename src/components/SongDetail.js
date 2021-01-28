9dda8a7f73447b6b69a05cb8b17ea8695e20d2bf
skynikita <yangluo1105@gmail.com>
2021-01-29 10:35:05 +1100

    UI update

---
+++ src/components/SongDetail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SongDetail = ({ song }) => {
+    if (!song){
+        return <div>Select a song!</div>
+    }
+    return ( <div>{song.title}
+              <div>
+               {song.duration}
+              </div>
+              </div>
+    )
+};
+
+
+const mapStateToProps = (state) => {
+    return { song: state.selectedSong}
+}
+
+export default connect( mapStateToProps)( SongDetail);
+
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,7 +12,7 @@ const songsReducer = () => {
 
 const selectedSongReducer = (selectedSong=null, action) => {
     if (action.type === 'SONG_SELECTED'){
-        return action.payloadType;
+        return action.payload;
     }
     return selectedSong;
 };
